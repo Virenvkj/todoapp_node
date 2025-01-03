6cfde51d7130a66e91122a9fc1d2bcc27e642360
Viren Gajjar <vgajjar1010@gmail.com>
2025-01-03 12:44:17 +0530

    feat: add api to create a new task

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const tasks = require('./mock_tasks.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 6069;
+
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.get('/tasks', (req, res) => {
@@ -10,6 +13,27 @@ app.get('/tasks', (req, res) => {
         <ul>${tasks.map((task) => `<li>${task.user_name}</li>`).join('')}</ul>
     `;
     return res.send(tasksHtml);
+});
+
+// Create a new task
+app.post('/api/tasks', (req, res) => {
+    const newTask = req.body;
+    const newTaskId = tasks.length + 1;
+    tasks.push({ ...newTask, id: newTaskId })
+    fs.writeFile('./mock_tasks.json', JSON.stringify(tasks), (err) => {
+        console.log(`Error adding new task to the file ${err}`);
+    });
+    return res.send({
+        "msg": `Task ${newTaskId} added successfully`,
+    });
+});
+
+// Edit an existing task
+app.patch('/api/tasks/:id', (req, res)=>{
+    const newTask = req.body;
+    // Edit the existing json at that id
+    // Update the mockData file with new json value
+    // Send successful response
 });
 
 // Get all the tasks
@@ -26,5 +50,5 @@ app.get('/api/tasks/:id', (req, res) => {
 
 // Listener
 app.listen(PORT, () => {
-    console.log("Darshan launched !")
+    console.log(`Darshan launched at ${PORT}!`)
 })
