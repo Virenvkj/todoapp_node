795765a36dc815f9647d4f073de2a50a8e940ecd
Viren Gajjar <vgajjar1010@gmail.com>
2025-01-08 12:18:01 +0530

    feat: add mongodb

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const tasks = require('./mock_tasks.json');
 const fs = require('fs');
+const mongoose = require('mongoose');
+const { type } = require('os');
 
 const app = express();
 const PORT = 6069;
+
+const todoSchema = mongoose.Schema({
+    user_name: {
+        type: String,
+    },
+    task_name: {
+        type: String,
+    },
+    completed: {
+        type: String,
+    },
+});
+
+const TodoModel = new mongoose.model('todos', todoSchema);
+
+mongoose.connect('mongodb://127.0.0.1:27017/my-todo')
+    .then(() => console.log('Mongo Connected')).catch((err) => console.log('Mongo Error'));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -23,16 +42,15 @@ app.get('/tasks', (req, res) => {
     return res.send(tasksHtml);
 });
 
-// Create a new task
-app.post('/api/tasks', (req, res) => {
-    const newTask = req.body;
-    const newTaskId = tasks.length + 1;
-    tasks.push({ ...newTask, id: newTaskId })
-    fs.writeFile('./mock_tasks.json', JSON.stringify(tasks), (err) => {
-        console.log(`Error adding new task to the file ${err}`);
-    });
-    return res.status(201).send({ "msg": `Task ${newTaskId} added successfully`, });
 
+app.post('/api/tasks', async (req, res) => {
+    const newTask = TodoModel({
+        user_name: req.body.user_name,
+        task_name: req.body.task_name,
+        completed: req.body.completed,
+    });
+    await newTask.save();
+    return res.status(201).send({ "msg": `Task added successfully`, });
 });
 
 // Edit an existing task
